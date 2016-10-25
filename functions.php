ad30f15d9d62e725db2116667fb2776880bdffe2
jamjar919 <james@mjsquare.com>
2016-10-25 15:55:39 +0000

    added password library, and got login to work

--- functions.php
+++ functions.php
@@ -1,12 +1,47 @@
 <?php
+ini_set('display_errors',1);  error_reporting(E_ALL);
 
 require 'db.php';
+require 'lib/password.php'; // Password hashing library
 
+/**
+* Translate a username to a UID
+*
+* @param string $username The username of the user you want to get a UID for
+*
+* @return integer The UID of the username supplied
+**/
+function getUid($username) {
+    try {
+        $db = new PDO('mysql:host='.DBSERV.';dbname='.DBNAME.';charset=utf8mb4', DBUSER, DBPASS);
+        $stmt = $db->prepare("SELECT * FROM user WHERE username = :name");
+        $stmt->bindValue(":name", $username, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (!empty($result)) {
+            return $result["uid"];
+        }
+        return false;
+    } catch(PDOEXCEPTION $e) {
+        // We errored out. 
+        return false;
+    }
+}
+
+
+/**
+* Check if a user is logged in. Mainly a wrapper for checkLoginToken
+*
+* @param array $s The session variable of the user - passing $_SESSION to this function is recommended. The session variable should have the "token" and "uid" properties
+*
+* @return boolean True if the user is logged in, False if the user is not
+**/
 function isLoggedIn($s) {
     if (isset($s["token"])) {
         if (isset($s["uid"])) {
             try {
-                return checkLoginToken($s["uid"],$s["token"]);
+                $value = checkLoginToken($s["uid"],$s["token"]);
+                return $value;
             } catch(PDOEXCEPTION $e) {
                 // We errored out. 
                 return false;
@@ -16,8 +51,17 @@ function isLoggedIn($s) {
     return false;
 }
 
+/**
+* Check the login token to make sure it is in date, correct for the user, and still valid.
+*
+* @param string $uid User id of the user you want to check
+* @param string $token Token you want to check if it is valid or not
+*
+* @return boolean True if the token is valid, False if the token is valid, or does not match the UID supplied
+**/
 function checkLoginToken($uid, $token) {
-    $stmt = $db->query("SELECT * FROM auth WHERE uid = :uid");
+    $db = new PDO('mysql:host='.DBSERV.';dbname='.DBNAME.';charset=utf8mb4', DBUSER, DBPASS);
+    $stmt = $db->prepare("SELECT * FROM auth WHERE uid = :uid");
     $stmt->bindValue(":uid", $uid, PDO::PARAM_INT);
     $stmt->execute();
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -35,4 +79,50 @@ function checkLoginToken($uid, $token) {
         }
     }
     return false;
+}
+
+/**
+* Generate a login token string, given a valid username and password. Checks against the database to make sure the password matches the one on record. Returns false otherwise
+*
+* @param string $username Username of user to generate a hash for
+* @param string $password Password of user to generate a hash for
+*
+* @return string The valid login token. Returns false if the data was invalid.
+*/
+function generateLoginToken($username, $password) {
+    $db = new PDO('mysql:host='.DBSERV.';dbname='.DBNAME.';charset=utf8mb4', DBUSER, DBPASS);
+    if (empty($username) || empty($password)) {
+        return false;
+    }
+    try {
+        $stmt = $db->prepare("SELECT * FROM user WHERE username = :username");
+        $stmt->bindValue(":username", $username, PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    } catch(PDOEXCEPTION $e) {
+        // We errored out. 
+        return false;
+    }
+    $hash = $result["password"];
+    if (password_verify($password, $hash)) {
+        // Valid! Make us a token!
+        $token = makeNewToken();
+        $expiry = time() + (7*24*60*60);
+        $uid = getUid($username);
+        // Store it in the database, with expiry date 1 week in the future.
+        $stmt = $db->prepare("INSERT INTO auth(authkey,uid,expiry) VALUES(:token,:uid,:expiry)");
+        $stmt->execute(array(':token' => $token, ':uid' => $uid, ':expiry' => $expiry));
+        return $token;
+    } else {
+        return false;
+    }
+}
+
+/**
+* Makes a new token, based on random data
+*
+* @returns string A random login token
+**/
+function makeNewToken() {
+    return md5((string)rand(0,100000));
 }
--- login.php
+++ login.php
@@ -1,18 +1,40 @@
 <?php
+ini_set('display_errors',1);  error_reporting(E_ALL);
 session_start();
 require 'functions.php';
 // Check if we are already logged in
 if (isLoggedIn($_SESSION)) {
-    // We are logged in, don't 
-    header("Location: members.php");
+    // We are logged in, go to members page
+    // header("Location: members.php");
+    echo "Automatically logged in via session";
+}
+// Did we submit the form? If we did, check if it's correct and generate a new login token
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+    echo "Form submitted";
+    $uid = getUid($_POST["username"]);
+    $token = generateLoginToken($_POST["username"], $_POST["password"]);
+    var_dump($token);
+    if ($token) { 
+        $_SESSION["uid"] = $uid;
+        $_SESSION["token"] = $token;
+        echo "Session cooie set";
+    } else {
+        // Login was incorrect!
+        $error = 'Your username or password was incorrect.';
+    }
 }
 ?>
 <!doctype html>
 <html>
     <head>
-        <title>Login</login>
+        <title>Login</title>
     </head>
     <body>
+        <?php
+            if (isset($error)) {
+                echo $error;
+            }
+        ?>
         <form class="login-form" action="#" method="POST">
             <label for="username">Username:</label>
             <input type="text" name="username">
