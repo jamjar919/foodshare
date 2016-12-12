03216af55e2cc0f1c6ecbaf52ace2b6f12b4d162
James Cockain <jamescockain@gmail.com>
2016-12-12 15:03:44 +0000

    sadsadsads

--- login.php
+++ login.php
@@ -1,4 +1,28 @@
-
+<?php
+session_start();
+require 'functions.php';
+// Check if we are already logged in
+var_dump($_SESSION);
+if (isLoggedIn($_SESSION)) {
+    // We are logged in, go to members page
+    // header("Location: members.php");
+    echo "Automatically logged in via session";
+    header("Location: index.php");
+}
+// Did we submit the form? If we did, check if it's correct and generate a new login token
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+    $uid = getUid($_POST["username"]);
+    $token = generateLoginToken($_POST["username"], $_POST["password"]);
+    if ($token) {
+        $_SESSION["uid"] = $uid;
+        $_SESSION["token"] = $token;
+        echo "Session set";
+    } else {
+        // Login was incorrect!
+        $error = 'Your username or password was incorrect.';
+    }
+}
+?>
 
 
 <!doctype html>
