95fcf9c718f1908d3f0689e10f86778b100911fb
James Paterson <james@mjsquare.com>
2017-03-24 17:23:50 +0000

    fix wacky indents

--- class/User.class.php
+++ class/User.class.php
@@ -6,11 +6,11 @@ require_once __ROOT__.'/lib/password.php'; // Password hashing library
 
 class User
 {
-    public $username;
-	public $authtoken;
-	private $isLoggedIn = false;
+        public $username;
+        public $authtoken;
+        private $isLoggedIn = false;
 
-	function __construct($user, $auth="") {
+        function __construct($user, $auth="") {
 		$this->username = $user;
 		if ($auth !== "") {
 			// User is trying to auth
@@ -26,7 +26,7 @@ class User
 	/**
 	* Sets the auth token as a cookie if password is correct
 	**/
-    public function login($password) {
+        public function login($password) {
         $token = $this->generateLoginToken($password);
 		if ($token === false) {
 			// Login unsuccessful
@@ -36,7 +36,7 @@ class User
 			$this->isLoggedIn = true;
 			$this->setUserCookie();
 		}
-    }
+        }
 	
 	public function setUserCookie() {
 		setcookie("token", $this->authtoken);
@@ -56,7 +56,7 @@ class User
 			return false;
 		}
 		try {
-            $db = new PDO('mysql:host='.DBSERV.';dbname='.DBNAME.';charset=utf8', DBUSER, DBPASS);
+                $db = new PDO('mysql:host='.DBSERV.';dbname='.DBNAME.';charset=utf8', DBUSER, DBPASS);
 			$stmt = $db->prepare("SELECT * FROM user WHERE username = :username");
 			$stmt->bindValue(":username", $username, PDO::PARAM_STR);
 			$stmt->execute();
@@ -172,104 +172,104 @@ class User
 	* Resend the verification email to the email address listed in the user profile, and generate new keys and that.
 	**/
 	private function reverifyEmail() {
-        try {
-            $db = new PDO('mysql:host='.DBSERV.';dbname='.DBNAME.';charset=utf8', DBUSER, DBPASS);
-            // Make a new verify token
-            $key = $this->makeNewToken();
-            $stmt = $db->prepare("UPDATE user SET confirm_email_key = :key WHERE username = :username");
-            $stmt->bindValue(":username", $this->username, PDO::PARAM_STR);
-            $stmt->bindValue(":key", $key, PDO::PARAM_STR);
-            $stmt->execute();
-            // Get user email
-            $stmt = $db->prepare("SELECT * FROM user WHERE username = :username");
-            $stmt->bindValue(":username", $this->username, PDO::PARAM_STR);
-            $stmt->execute();
-            $row = $stmt->fetch();
-            $email = $row["email"];
-            // Send confirmation mail
-            mail($email, "Email Change", "Hey! \n \n You changed your email to this new address. Click the link below to confirm that you own this email address: \n \n <a href=\"".DOMAIN."confirm.php?key=".$key."&user=".$this->username."\">Confirm registration</a> \n \n Thanks, \n The FoodShare Team");
-            return true;
-        } catch(PDOException $ex) {
-            return false;
-        }
+                try {
+                        $db = new PDO('mysql:host='.DBSERV.';dbname='.DBNAME.';charset=utf8', DBUSER, DBPASS);
+                        // Make a new verify token
+                        $key = $this->makeNewToken();
+                        $stmt = $db->prepare("UPDATE user SET confirm_email_key = :key WHERE username = :username");
+                        $stmt->bindValue(":username", $this->username, PDO::PARAM_STR);
+                        $stmt->bindValue(":key", $key, PDO::PARAM_STR);
+                        $stmt->execute();
+                        // Get user email
+                        $stmt = $db->prepare("SELECT * FROM user WHERE username = :username");
+                        $stmt->bindValue(":username", $this->username, PDO::PARAM_STR);
+                        $stmt->execute();
+                        $row = $stmt->fetch();
+                        $email = $row["email"];
+                        // Send confirmation mail
+                        mail($email, "Email Change", "Hey! \n \n You changed your email to this new address. Click the link below to confirm that you own this email address: \n \n <a href=\"".DOMAIN."confirm.php?key=".$key."&user=".$this->username."\">Confirm registration</a> \n \n Thanks, \n The FoodShare Team");
+                        return true;
+                } catch(PDOException $ex) {
+                        return false;
+                }
 	}
 	
 	
 	/**
 	* Update the listed email address. Returns FALSE on failure. Also triggers a verification update.
 	**/
-    public function updateEmail($email) {
-        if ($this->isLoggedIn()) {
-            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                try {
-                    $db = new PDO('mysql:host='.DBSERV.';dbname='.DBNAME.';charset=utf8', DBUSER, DBPASS);
-                    $stmt = $db->prepare("UPDATE user SET verified = 0, email = :email WHERE username = :username");
-                    $stmt->bindValue(":username", $this->username, PDO::PARAM_STR);
-                    $stmt->bindValue(":email", $email, PDO::PARAM_STR);
-                    $stmt->execute();
-                    if ($stmt->rowCount()) {
-                        $this->reverifyMail();
-                        return true;
-                    }
-                    return false;
-                } catch(PDOException $ex) {
-                    return false;
+        public function updateEmail($email) {
+                if ($this->isLoggedIn()) {
+                        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                                try {
+                                        $db = new PDO('mysql:host='.DBSERV.';dbname='.DBNAME.';charset=utf8', DBUSER, DBPASS);
+                                        $stmt = $db->prepare("UPDATE user SET verified = 0, email = :email WHERE username = :username");
+                                        $stmt->bindValue(":username", $this->username, PDO::PARAM_STR);
+                                        $stmt->bindValue(":email", $email, PDO::PARAM_STR);
+                                        $stmt->execute();
+                                        if ($stmt->rowCount()) {
+                                                $this->reverifyMail();
+                                                return true;
+                                        }
+                                        return false;
+                                } catch(PDOException $ex) {
+                                        return false;
+                                }
+                        }
                 }
-            }
+                return false;
         }
-        return false;
-	}
 	
 	/**
 	* Expires all login tokens for the user! This will effectively log them out everywhere.
 	**/
-	public function expireLoginTokens() {
-        if ($this->isLoggedIn()) {
-            try {
-                $db = new PDO('mysql:host='.DBSERV.';dbname='.DBNAME.';charset=utf8', DBUSER, DBPASS);
-                $stmt = $db->prepare("DELETE FROM auth WHERE username = :username");
-                $stmt->bindValue(":username", $this->username, PDO::PARAM_STR);
-                $stmt->execute();
-                return true;
-            } catch(PDOException $ex) {
+        public function expireLoginTokens() {
+                if ($this->isLoggedIn()) {
+                        try {
+                                $db = new PDO('mysql:host='.DBSERV.';dbname='.DBNAME.';charset=utf8', DBUSER, DBPASS);
+                                $stmt = $db->prepare("DELETE FROM auth WHERE username = :username");
+                                $stmt->bindValue(":username", $this->username, PDO::PARAM_STR);
+                                $stmt->execute();
+                                return true;
+                        } catch(PDOException $ex) {
+                                return false;
+                        }
+                }
                 return false;
-            }
         }
-        return false;
-	}
 	
     /**
     * Changes the user password. Returns FALSE on failure. Will also expire all user tokens!
     **/
 	public function updatePassword($password) {
-        if ($this->isLoggedIn()) {
-            // Check for dumb cases
-            if (empty($password)) {
-                return false;
-            }
-            if (strlen($password) < 3) {
-                return false;
-            }
-            // Generate hash
-            $hash = password_hash($password, PASSWORD_BCRYPT);
-            // Actually change the pw
-            try {
-                $db = new PDO('mysql:host='.DBSERV.';dbname='.DBNAME.';charset=utf8', DBUSER, DBPASS);
-                $stmt = $db->prepare("UPDATE user SET password = :pass WHERE username = :username");
-                $stmt->bindValue(":username", $this->username, PDO::PARAM_STR);
-                $stmt->bindValue(":pass", $hash, PDO::PARAM_STR);
-                $stmt->execute();
-                if ($stmt->rowCount()) {
-                    $this->expireLoginTokens();
-                    return true;
+                if ($this->isLoggedIn()) {
+                        // Check for dumb cases
+                        if (empty($password)) {
+                                return false;
+                        }
+                        if (strlen($password) < 3) {
+                                return false;
+                        }
+                        // Generate hash
+                        $hash = password_hash($password, PASSWORD_BCRYPT);
+                        // Actually change the pw
+                        try {
+                                $db = new PDO('mysql:host='.DBSERV.';dbname='.DBNAME.';charset=utf8', DBUSER, DBPASS);
+                                $stmt = $db->prepare("UPDATE user SET password = :pass WHERE username = :username");
+                                $stmt->bindValue(":username", $this->username, PDO::PARAM_STR);
+                                $stmt->bindValue(":pass", $hash, PDO::PARAM_STR);
+                                $stmt->execute();
+                                if ($stmt->rowCount()) {
+                                        $this->expireLoginTokens();
+                                        return true;
+                                }
+                                return false;
+                        } catch(PDOException $ex) {
+                                return false;
+                        }
                 }
                 return false;
-            } catch(PDOException $ex) {
-                return false;
-            }
         }
-        return false;
-	}
 	
 }
 ?>
