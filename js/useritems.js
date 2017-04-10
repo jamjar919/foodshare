3545b337e1a2f622bd1c452b5ef95c5a91006e1b
James Paterson <james@mjsquare.com>
2017-04-11 00:36:58 +0100

    seperate files

--- js/useritems.js
+++ js/useritems.js
@@ -13,39 +13,3 @@ function getUserItems(username,sort="Most recent",num=10) {
         });
     });
 }
-/*
- * Prints items as a card into the specified element
- */
-function printFoodItems(items, element) {
-    for (var i = 0; i< items.length; i++) {
-        item = items[i];
-        $(element).append(
-            $("<div>")
-            .addClass("card food-item")
-            .append(item["image_url"] ? '<img class="card-img-top" src="'+item["image_url"]+'">' : '')
-            .append(
-                $("<div>")
-                .addClass("card-block")
-                .append(
-                    $("<h4>").text(item["name"])
-                )
-                .append(
-                    $("<p>")
-                    .addClass("card-subtitle mb-2 text-muted")
-                    .text("Expires "+moment(item["expiry"]).fromNow())
-                )
-                .append(
-                    $("<p>")
-                    .addClass("card-text")
-                    .text(item["description"].substring(0,300))
-                )
-                .append(
-                    $("<a>")
-                    .attr("href","edititem.php?item="+item["id"])
-                    .addClass("btn btn-primary")
-                    .text("Edit")
-                )
-            )
-        );
-    }
-}
