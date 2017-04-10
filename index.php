879721cfd11916bed5f021b4a61c7650c638f0de
James Paterson <james@mjsquare.com>
2017-04-10 20:13:23 +0100

    better

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
         <div class="card">
             <img class="card-img-top" src="http://lorempixel.com/400/200/food/">
             <div class="card-block">
-                <h4 class="card-title">Card title</h4>
+                <h4 class="card-title">Card title really really really really really really really really really really long</h4>
                 <p class="card-text">This defines the alignment along the main axis. It helps distribute extra free space left over when either all the flex items on a line are inflexible, or are flexible but have reached their maximum size. It also exerts some control over the alignment of items when they overflow the line..</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
@@ -44,7 +44,7 @@
         </div>
         <div class="card">
             <div class="card-block">
-                <h4 class="card-title">Card title</h4>
+                <h4 class="card-title">Card without an image header</h4>
                 <p class="card-text">This defines the default behaviour for how flex items are laid out along the cross axis on the current line. Think of it as the justify-content version for the cross-axis (perpendicular to the main-axis).</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
