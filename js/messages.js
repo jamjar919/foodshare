974ee3e44569ee56c6325a4dcd9f70ebcd20fc4c
James Paterson <james@mjsquare.com>
2017-04-11 16:27:25 +0100

    add message js and example code

---
+++ js/messages.js
@@ -0,0 +1,27 @@
+var messageSettings = {
+    sentTo: 'defaultto',
+    from: 'defaultfrom',
+    container: '#message-container'
+}
+function addMessage(text,timestamp,isOwnMessage = false) {
+    $(messageSettings.container).append(
+        $("<div>")
+        .addClass("card message")
+        .addClass(isOwnMessage ? "to" : "from")
+        .append(
+            $("<div>")
+            .addClass("card-header")
+            .text(isOwnMessage ? messageSettings.from : messageSettings.sentTo)
+            .append(
+                $("<span>")
+                .addClass("converttime message-timestamp")
+                .text(timestamp)
+            )
+        )
+        .append(
+            $("<div>")
+            .addClass("card-block")
+            .text(text)
+        )
+    );
+}
--- messages.php
+++ messages.php
@@ -6,7 +6,29 @@
     $p->buildHeader();
     $profile = $p->user->getPrivateProfile();
 ?>
-
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="card">
+                <div class="card-header text-center">
+                    Messenger
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">User 1</li>
+                    <li class="list-group-item">User 2</li>
+                    <li class="list-group-item">User 3</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-sm-9 messages" id="message-container">
+        </div>
+    </div>
+    <script>
+        $(document).ready(function() {
+            addMessage("Example message from someone", "2014-11-22 12:45:34", false)
+            addMessage("Example message to someone", "2014-11-22 12:45:34", true)
+        })
+    </script>
+    <script src="js/messages.js"></script>
 <?php
     $p->buildFooter();
 ?>
