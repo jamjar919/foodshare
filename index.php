ec9bc603c5c90e645406977392363b061e4fcc3f
James Paterson <james@mjsquare.com>
2017-04-12 01:04:17 +0100

    frontpage remove searchbar

--- index.php
+++ index.php
@@ -10,18 +10,6 @@
         <p><a class="btn btn-primary btn-lg" href="register.php" role="button">Sign up now!</a></p>
     </div>
     <h2>See what's on offer...</h2>
-    <div class="inline-searchbar">
-        <div class="input-group">
-            <input class="form-control" type="text" placeholder="Search food">
-            <div class="input-group-btn">
-                <div class="btn-group" role="group">
-                    <div class="dropdown dropdown-lg">
-                        <button id="search" class="btn btn-custom"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="masonry" id="frontpageitems">
     </div>
     <script>
