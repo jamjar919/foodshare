63dec041f46d266812e5a5685d87b73f4e28bc64
James Paterson <james@mjsquare.com>
2017-04-18 22:56:47 +0100

    fixed issue #13

--- class/Food.class.php
+++ class/Food.class.php
@@ -334,7 +334,7 @@ class Food
             return false;
         }
         // Send email!
-        mail(UserTools::getEmail($this->owner), "Item claimed!", "Hey! \n \n An item you put up, titled \"".$this->item["title"]."\" has been claimed by the user ".$claimer."! They should be in contact via the messaging system soon to arrange a pickup time. \n Thanks, \n FlavourTown");
+        mail(UserTools::getEmail($this->owner), "Item claimed!", "Hey! \n \n An item you put up, titled \"".$this->item["name"]."\" has been claimed by the user ".$claimer."! They should be in contact via the messaging system soon to arrange a pickup time. \n\n Thanks, \n FlavourTown");
         return $result;
    }
         
