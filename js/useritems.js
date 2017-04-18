e2379cc7a6ee0afabac208fdc583aa22be0fe6a6
James Paterson <james@mjsquare.com>
2017-04-18 23:47:19 +0100

    ie didn't even let me know this was crashing it

--- js/useritems.js
+++ js/useritems.js
@@ -1,5 +1,6 @@
 var endpoint = "api/profile/food.php"
-function getUserItems(username,sort="Most recent",num) {
+function getUserItems(username,sort,num) {
+    sort = sort || "Most recent";
     num = num || 10;
     return new Promise(function(resolve, reject) {
         $.get(endpoint, {username: username, sort: sort, num: num})
