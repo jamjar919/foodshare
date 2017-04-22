fffa1ca1b01b12c29fd878f544aa2c271c814dd3
Vaibhav Agarwal <vaibhavag112@gmail.com>
2017-04-22 11:48:54 +0100

    Added faqs

--- help.php
+++ help.php
@@ -12,28 +12,28 @@
 ?>
     <div class="row">
         <div id="collapsible-panels" class="col-md-6">
-                <h1>FAQ's</h1>
-            <h2><a href="#">Question</a></h2>
+                <h1>FAQs</h1>
+            <h2><a href="#">How do I add a new item?</a></h2>
             <div class="card card-block">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text">To add a new food item, go to your profile page and simply click on the add item button and fill out the required details.</p>
             </div>
-            <h2><a href="#">Question</a></h2>
+            <h2><a href="#">How do I claim a food item?</a></h2>
             <div class="card card-block">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text">To claim a food item, find an item of your choice and simply click on the claim button.</p>
             </div>
-            <h2><a href="#">Question</a></h2>
+            <h2><a href="#">How do I arrange for a pickup?</a></h2>
             <div class="card card-block">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text">Once you have claimed a food item, you will be directed to the messages area where you can send messages to the item's owner and arrange for a pickup date and time.</p>
             </div>
-            <h2><a href="#">Question</a></h2>
+            <h2><a href="#">Can I change my profile details?</a></h2>
             <div class="card card-block">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text">Yes you can! Just click on the edit profile button and make the necessary changes.</p>
             </div>
         </div>
         <div class="col-md-6">
             <div class="jumbotron">
-                <h2>Still more questions?</h2>
-                <p>Email us at <a href="mailto:gofuckyourself@flavourtown.com">gofuckyourself@flavourtown.com</a></p>
+                <h2>Have a question which is not answered above?</h2>
+                <p>Email us at <a href="mailto:cs-seg1@durham.ac.uk">help@flavourtown.co.uk</a></p>
             </div>
         </div>
     </div>
